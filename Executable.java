e5d77a4519cee51fba4c676afef43689132571c9
fusuykite <yusuf.bahadur@gmail.com>
2017-11-21 00:41:58 -0800

    Delete Executable

--- Executable.java
+++
@@ -1,7 +0,0 @@
-public interface Executable extends EntityInterface  {
-
-    public void execute(WorldModel world, ImageStore imageStore, EventScheduler scheduler);
-
-    public void scheduleActions(EventScheduler scheduler, WorldModel world, ImageStore imageStore);
-
-}
